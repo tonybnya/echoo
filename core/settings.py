b90611c3f32a87b875e023290959c4f8ef150761
Tony Blondeau NYA <tonybnya@gmail.com>
2026-01-21 22:31:42 +0100

    perf: configure third-party apps DRF & djoser

--- core/settings.py
+++ core/settings.py
@@ -31,12 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third-party apps
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    # my apps
+    'chat'
 ]
 
 MIDDLEWARE = [
