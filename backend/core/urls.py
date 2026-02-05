ac79638306f7ea0885c8004c8a0692e596a61e1e
Tony Blondeau NYA <tonybnya@gmail.com>
2026-02-05 15:11:41 +0100

    docs: comment djoser urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # djoser URLs for authentication
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
