3c7df3e932559b7d03c4c71a727fc87d4eee6179
Tony Blondeau NYA <tonybnya@gmail.com>
2026-02-05 20:01:17 +0100

    feat: manage routing between UserAuth and Chat components

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,23 +1,43 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+// import HomeView from '../views/HomeView.vue';
+import Chat from '@/components/Chat.vue';
+import UserAuth from '@/components/UserAuth.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView,
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue'),
+    // },
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/chat',
+      name: 'Chat',
+      component: Chat
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+      path: '/auth',
+      name: 'UserAuth',
+      component: UserAuth
+    }
   ],
 })
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (sessionStorage.getItem('authToken') !== null || to.path === '/auth') {
+    next()
+  } else {
+    next('/auth')
+  }
+})
+
+export default router;
