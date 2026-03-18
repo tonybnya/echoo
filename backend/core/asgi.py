71ff26798b7e9b73e7763aedb8f6cb9896d0be3b
tonybnya <tonybnya@gmail.com>
2026-03-18 18:10:05 +0100

    fix: add django.setup() to asgi.py

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -11,6 +11,10 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
+import django
+
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
