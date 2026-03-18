7e3c177486c2c343bad694af09b6e38e71300c5f
tonybnya <tonybnya@gmail.com>
2026-03-18 18:04:57 +0100

    fix Django settings loading order in asgi.py

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -4,22 +4,21 @@ ASGI config for core project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/5.2/topics/settings/
 """
 
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
+    }
+)
